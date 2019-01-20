e9b9321a8d4772aec9b04043951aba6c76637531
joodongkim <fdragons5@gmail.com>
2019-01-20 11:08:20 +0900

    cvbn

--- src/hijava/practice/ex3_3.java
+++ src/hijava/practice/ex3_3.java
@@ -3,25 +3,23 @@ package hijava.practice;
 public class ex3_3 {
 
 	public static void main(String[] args) {
+		int total = 0;
 		
-		for(int i=1; i<=100; i++) {
+		for(int i=2; i<=100; i++) {
 			boolean isPrime = true;
-			for(int j=2; j<=(i-1); j++) {
-				
-				if(isPrime) {
-					
-				}
-				
+			for(int j=2; j<i; j++) {
 				if(i%j==0) {
 					isPrime=false;
-					break;
+					break;	
 					
 					}
 			
-System.out.println(i);
 			}
-			
+			if(isPrime)
+				total += i;
+				
 		}
+		System.out.println("total = " + total);
 	}
 
 }
